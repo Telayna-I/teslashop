541a59658b108010a8191476a554e052272634c2
Telayna-I <telayna98@gmail.com>
2024-11-23 16:32:05 -0300

    demo

--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -35,7 +35,7 @@ export const LoginForm = () => {
 				className='px-5 py-2 border bg-gray-200 rounded mb-5'
 				name='password'
 				type='password'
-				defaultValue='123456'
+				// defaultValue='123456'
 			/>
 			<div className='flex h-8 items-end space-x-1' aria-live='polite' aria-atomic='true'>
 				{state === "CredentialsSignin" && (
